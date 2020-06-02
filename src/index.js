93248bb335571fd078f20ad5876fc2ad17776022
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-06-02 18:59:30 +0530

    takeaway

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 
+function onScroll(event) {
+  const scrolledTo = window.innerHeight + window.pageYOffset;
+  const scrollLimit = document.body.offsetHeight;
+  const scrollThreshold = 30;
+  ​
+  if (scrollLimit - scrolledTo <= scrollThreshold) {
+  // TODO update
+  }
+  }
+  
+  ​
+  window.addEventListener('scroll', onScroll);
+  
